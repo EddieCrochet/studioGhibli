28a41a22a070d350b0f541a817591f50a8f8d01d
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-04-22 20:59:57 -0500

    this code got me to get the api with no problem, - play around time from here out

--- scripts.js
+++ scripts.js
@@ -5,9 +5,34 @@ fetch('https://ghibliapi.herokuapp.com/films')
 .then((data) => {
     //work with data here
     console.log(data)
+    data.forEach((movie) => {
+        //creating a div with the card class
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+
+        // create an h1 and set the text content to the film's title
+        const h1 = document.createElement('h1');
+        h1.textContent = movie.title;
+
+        // create a p and set the text content to the film's description
+        const p = document.createElement('p');
+        movie.description = movie.description.substring(0, 3000);
+        p.textContent = `${movie.description}...`;
+
+        // Append the cards to the container element
+        container.appendChild(card);
+
+        // Each child will contain an h1 and a p
+        card.appendChild(h1);
+        card.appendChild(p);
+    })
 })
 .catch((err) => {
     //Do something with an error here
+    const errorMessage = document.createElement('section');
+    errorMessage.setAttribute('class', 'errorSect');
+    errorMessage.textContent = 'Gahsh dahg nahbbit! Aint wrkin.';
+    app.appendChild(errorMessage);
 })
 
 const app = document.getElementById('root');
