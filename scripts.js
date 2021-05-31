da75dc48537c2e429266080561ad990ac508a02a
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-05-31 12:59:26 -0500

    added all available info to individual filtered movie page

--- scripts.js
+++ scripts.js
@@ -33,28 +33,43 @@ fetch('https://ghibliapi.herokuapp.com/films')
     let cards = document.getElementsByClassName("card");
     for(let i = 0; i < cards.length; i++) {
         cards[i].addEventListener('click', function(){
+            //creating a div with the card class
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card');
+
             let curID = data[i].id;
             let filteredMovie = data.filter(movie => movie.id === curID)[0];
             // clicked on movie from the API is stored in var above
 
             container.innerHTML = "";
-            // cleared th container.
+            // cleared the container.
             // now we need to add nodes and info for selected movie
 
             const title = document.createElement('h1');
-            const originalTitle = document.createElement('h2');
-            const romanisedTitle = document.createElement('h2');
+            const originalTitle = document.createElement('h3');
+            const romanisedTitle = document.createElement('h3');
+            const director = document.createElement('h4');
+            const producer = document.createElement('h4');
+            const releaseYear = document.createElement('h4');
+            const rtScore = document.createElement('h4');
+            const runTime = document.createElement('h4');
+            const desc = document.createElement('p');
 
             console.log(filteredMovie);
 
             title.textContent = filteredMovie.title;
             originalTitle.textContent = filteredMovie.original_title;
+            romanisedTitle.textContent = `(${filteredMovie.original_title_romanised})`;
+            director.textContent = `Directed by: ${filteredMovie.director}`;
+            producer.textContent = `Produced by: ${filteredMovie.producer}`;
+            releaseYear.textContent = `Released: ${filteredMovie.release_date}`;
+            rtScore.textContent = `Rotten Tomatoes Score: ${filteredMovie.rt_score}`;
+            runTime.textContent = `Run Time: ${filteredMovie.running_time} minutes`;
+            desc.textContent = filteredMovie.description;
 
-            console.log(title);
-            console.log(originalTitle);
+            card.append(title, originalTitle, romanisedTitle, director, producer, releaseYear, rtScore, runTime, desc);
 
-            container.appendChild(title);
-            container.appendChild(originalTitle);
+            container.appendChild(card);
         });
     }
 })
